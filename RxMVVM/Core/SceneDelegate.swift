9d7b536c89d4cccad15ba61d3e754eb257cf432f
Moataz-Ashraf <55600770+Moataz-Ashraf@users.noreply.github.com>
2024-12-23 23:27:10 +0200

    update missing files

--- RxMVVM/Core/SceneDelegate.swift
+++ RxMVVM/Core/SceneDelegate.swift
@@ -11,10 +11,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
-           // var contentView : UIViewController?
             let window = UIWindow(windowScene: windowScene)
             
             let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
@@ -28,34 +26,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
+
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        // Handle scene disconnection.
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        // Restart any paused tasks.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        // Pause active tasks or disable updates.
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        // Undo background changes.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        // Save data and release shared resources.
     }
-
-
 }
-
